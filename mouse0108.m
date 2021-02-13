fbf3ab5684395fdb7ba141f7ebe57c2670006ac8
Deborah Ho <47065259+debho@users.noreply.github.com>
2021-02-12 20:36:48 -0500

    Add files via upload

--- mouse0108.m
+++ mouse0108.m
@@ -1,6 +1,6 @@
 % reads data into matrices
-data = readmatrix('20210108_data.csv');
-type = readmatrix('20210108_type.csv');
+% data = readmatrix('20210108_data.csv');
+% type = readmatrix('20210108_type.csv');
 
 % extracts EEG data
 % finds the indices of each type in the type matrix and finds corresponding
@@ -12,8 +12,8 @@ eeg1 = data(idx1);
 idx2 = type == 3;
 eeg2 = data(idx2);
 
-idx3 = type == 4;
-eeg3 = data(idx3);
+% idx3 = type == 4;
+% eeg3 = data(idx3);
 
 idx4 = type == 5;
 eeg4 = data(idx4);
@@ -21,10 +21,10 @@ eeg4 = data(idx4);
 % power spectrum with FFT from 1-100Hz
 
 fs = 250; % EEG was sampled at 250Hz
-eeg1 = array2timetable(fft(eeg1'), 'SampleRate', fs);
-eeg2 = array2timetable(fft(eeg2'), 'SampleRate', fs);
-eeg3 = array2timetable(fft(eeg3'), 'SampleRate', fs);
-eeg4 = array2timetable(fft(eeg4'), 'SampleRate', fs);
+eeg1 = array2timetable(eeg1', 'SampleRate', fs);
+eeg2 = array2timetable(eeg2', 'SampleRate', fs);
+% eeg3 = array2timetable(eeg3', 'SampleRate', fs);
+eeg4 = array2timetable(eeg4', 'SampleRate', fs);
 eeg = synchronize(eeg1,eeg2,eeg3,eeg4);
 eeg.Properties.VariableNames = ["EEG 1", "EEG 2", "EEG 3", "EEG 4"];
 
@@ -33,15 +33,16 @@ pspectrum(eeg1, "FrequencyLimits", [0 100])
 hold on 
 pspectrum(eeg2, "FrequencyLimits", [0 100])
 hold on 
-pspectrum(eeg3, "FrequencyLimits", [0 100])
-hold on
+% pspectrum(eeg3, "FrequencyLimits", [0 100])
+% hold on
 pspectrum(eeg4, "FrequencyLimits", [0 100])
-legend("EEG 1", "EEG 2", "EEG 3", "EEG 4")
+legend("Parietal", "Frontal", "EMG")
 xlabel("Frequency (Hz)")
 ylabel("Power")
 hold off
 
-% spectrograms
-pspectrum(eeg1, eeg(:,"Time"), "spectrogram", "FrequencyLimits", [0 100])
-% ^ keeps giving me an error message, not sure how to fix it
-
+% spectrogram
+pspectrum(eeg1, "spectrogram", "FrequencyLimits", [0 100])
+colormap(jet)
+caxis auto
+ylim([0 35])
