c4449227259703dae8b08aed2eac8151ac17a1ea
Matt Gaidica <matt@gaidi.ca>
2021-02-13 10:17:16 -0500

    extract behaviors from binary file

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 
-# MATLAB
-## Stuff I figured out:
+# BORIS-Ephys Tools
+[BORIS User Guide](https://boris.readthedocs.io/en/latest/)
+###Exporting from BORIS:
+1. Use "as behaviors binary table"
+2. Use 1-second intervals for now
+3. Export as .csv
+
+*Always include the date in filenames as YYYYMMDD
+
+---
+### Stuff I figured out:
 - How to extract data by type
 - Creating power spectrum and spectrogram
-## Open Issues:
+
+### Open Issues:
 - I'm not sure if I plotted the power spectra correctly
 - Can't seem to work spectrogram code --> I think this is where I need help getting the data into 5-sec bins
---
+++ extractBinaryBehaviors.m
@@ -0,0 +1,59 @@
+% right now, time interval = 1s, so indexing is easy...
+% if data is exported with greater resolution, the behTime matrix would
+% need to be referenced OR the event times in behExtract would need to be
+% converted from index to time
+
+function [behNames,behTime,behExtract,extractedLabels,binBeh] = extractBinaryBehaviors(filename,doPlot)
+
+fid = fopen(filename);
+behNames = strsplit(fgetl(fid),',');
+behNames = behNames(2:end); % remove time label
+fclose(fid);
+binBeh = readmatrix(filename,'NumHeaderLines',1);
+behTime = binBeh(:,1); % save time
+binBeh(:,1) = []; % remove time data
+
+extractedLabels = {'EventId','EventStart','EventEnd','Duration'};
+behExtract = [];
+for iBeh = 1:size(binBeh,2) % 1 is time
+    thisBehData = logical(binBeh(:,iBeh)); % point events can be >1, force binary
+    eventsStart = find(diff(thisBehData) == 1);
+    eventsEnd = find(diff(thisBehData) == -1);
+    
+    useRange = size(behExtract,1) + 1:size(behExtract,1) + numel(eventsStart);
+    behExtract(useRange,1) = repmat(iBeh,[numel(eventsStart),1]);
+    behExtract(useRange,2) = eventsStart;
+    behExtract(useRange,3) = eventsEnd;
+    behExtract(useRange,4) = eventsEnd-eventsStart;
+end
+
+if ~doPlot, return, end
+%%
+colors = lines(size(binBeh,2));
+h = figure('position',[0 0 1000 500]);
+for iBeh = 1:numel(behNames)
+    % imported data
+    xData = find(binBeh(:,iBeh)==1);
+    plot(xData,repmat(iBeh,size(xData)),'.','markerSize',10,'color',colors(iBeh,:));
+    hold on;
+    % exorted data
+    thisEventData = behExtract(behExtract(:,1)==iBeh,:);
+    for iEx = 1:size(thisEventData,1)
+        plot(thisEventData(iEx,2),iBeh,'o','markerSize',10,'color',colors(iBeh,:)); % open
+        plot(thisEventData(iEx,3),iBeh,'x','markerSize',10,'color',colors(iBeh,:)); % close
+    end
+end
+title('Imported and Extracted Behaviors');
+ax = gca;
+ax.YGrid = 'on';
+ax.FontSize = 14;
+set(gcf,'color','w');
+yticks(1:size(binBeh,2));
+ylim([0,size(binBeh,2)+1]);
+yticklabels(behNames);
+xlim([behTime(1),behTime(end)]);
+xlabel('time (s)');
+
+saveas(h,strrep(filename,'.csv','.png'));
+
+% remember to adjust times to match ephys
