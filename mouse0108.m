1e900d8fb0d75dcfc75ccf21d7b3d6d592d35118
Deborah Ho <47065259+debho@users.noreply.github.com>
2021-02-16 18:00:14 -0500

    pasted code

--- mouse0108.m
+++ mouse0108.m
@@ -17,19 +17,19 @@ eeg4 = data(idx4);
 
 % converts matrices to timetables
 fs = 250; % EEG was sampled at 250Hz
-eeg1 = array2timetable(eeg1', 'SampleRate', fs);
-eeg2 = array2timetable(eeg2', 'SampleRate', fs);
-eeg3 = array2timetable(eeg3', 'SampleRate', fs);
-eeg4 = array2timetable(eeg4', 'SampleRate', fs);
-eeg = synchronize(eeg1,eeg2,eeg3,eeg4); % puts data from all 4 contacts into one table
-eeg.Properties.VariableNames = ["Parietal", "Frontal", " ", "EMG"];
+%eeg1 = array2timetable(eeg1', 'SampleRate', fs);
+%eeg2 = array2timetable(eeg2', 'SampleRate', fs);
+%eeg3 = array2timetable(eeg3', 'SampleRate', fs);
+%eeg4 = array2timetable(eeg4', 'SampleRate', fs);
+%eeg = synchronize(eeg1,eeg2,eeg3,eeg4); % puts data from all 4 contacts into one table
+%eeg.Properties.VariableNames = ["Parietal", "Frontal", " ", "EMG"];
 
 % spectrogram
-pspectrum(eeg1, "spectrogram", "FrequencyLimits", [0 100])
-colormap(jet)
-caxis auto
-ylim([0 35])
-hold off
+%pspectrum(eeg1, "spectrogram", "FrequencyLimits", [0 100])
+%colormap(jet)
+%caxis auto
+%ylim([0 35])
+%hold off
 
 % extracting binary behaviors
 [behNames,behTime,behExtract,extractedLabels,binBeh] = extractBinaryBehaviors('boris_binary_20210108_mouse.csv',true);
@@ -51,13 +51,28 @@ hold off
 %hold off
 
 % plotting power spectra with FFT on 5-sec bins
-for index = behRanges(1:end,2:3)
-    tstart = behRanges(:,2) * fs;
-    tend = behRanges(:,3) * fs;
-    pspec = data(tstart:tend);
-    pspec = array2timetable(pspec', "SampleRate", fs);
+% split by behaviors (column 1) so that we can compare means
+
+Parr = [];
+eeg1Beh = find(behRanges(:,1) == 2); % eeg1
+for ii = 1:numel(eeg1Beh)
+  tstart = (behRanges(eeg1Beh(ii),2) - 1) * fs;
+  tend = behRanges(eeg1Beh(ii),3) * fs;
+   pspec = array2timetable(eeg1.Var1(tstart:tend), "SampleRate", fs);
+   [P,F] = pspectrum(pspec, "FrequencyLimits", [0 100]);
+   Parr(ii,:) = 10*log10(P);
 end
-pspectrum(pspec, "FrequencyLimits", [0 100])
+figure;
+plot(F, mean(Parr));
+% stats - Parr has 147 different values, you can extract freq band like:
+bandPowers = Parr(:,F>1 & F<4);
+% take mean of dim=2
+bandPower = mean(bandPowers,2);
+% make sure it's relatively stable across each bin
+figure;
+plot(bandPower);
+% todo: now compare these values with another behavior
+% using anova1();
 
 
 
