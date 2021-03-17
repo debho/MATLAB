b8649392da6aba1d56377be4cb8e220652145102
Deborah Ho <47065259+debho@users.noreply.github.com>
2021-03-17 17:55:04 -0400

    added in code to generate power spectra

--- mouse0311.m
+++ mouse0311.m
@@ -8,7 +8,7 @@ end
     'boris_binary_20210311_mouse.csv'),0,-17,false);
 behRanges = binBehaviors(binBeh,behTime,5,false);
 
-% ch1(type=2): ?, ch2 (type=3): ?, ch3(type=4): ?, ch4(type=5): ?
+% ch1(type=2): EMG, ch2 (type=3): ?, ch3(type=4): ?, ch4(type=5): ?
 fs = 241;
 close all
 figure('position',[0 0 1400 1000]);
@@ -50,6 +50,59 @@ for iType = 2:5
     yticks([]);
     drawnow;
 end
+
+% power spectra
+% sleep
+Parr = [];
+eeg_parBeh = find(behRanges(:,1) == 3);
+for ii = 1:numel(eeg_parBeh)
+    tstart = round(behRanges(eeg_parBeh(ii),2) * fs);
+    tend = round(behRanges(eeg_parBeh(ii),3) * fs);
+    pspec = array2timetable(eeg_t.Var1(tstart:tend), "SampleRate", fs);
+    [P,F] = pspectrum(pspec, "FrequencyLimits", [0 100]);
+    Parr(ii,:) = 10*log10(P);
+end
+
+hSpectrum = figure;
+plot(F, mean(Parr));
+hold on; % for others!
+xlabel("Frequency (Hz)")
+ylabel("Mean Power")
+title("Mean Power against Frequency (Sleep)")
+% extract freq band
+bandPowers = Parr(:,F>1 & F<4);
+% take mean of dim=2
+bandPower = mean(bandPowers,2);
+% make sure it's relatively stable across each bin
+figure;
+plot(bandPower);
+title("Mean Power at 2Hz (Sleep)")
+
+% wake-still
+Parr = [];
+eeg_parBeh = find(behRanges(:,1) == 5);
+for ii = 1:numel(eeg_parBeh)
+    tstart = round(behRanges(eeg_parBeh(ii),2) * fs);
+    tend = round(behRanges(eeg_parBeh(ii),3) * fs);
+    pspec = array2timetable(eeg_t.Var1(tstart:tend), "SampleRate", fs);
+    [P,F] = pspectrum(pspec, "FrequencyLimits", [0 100]);
+    Parr(ii,:) = 10*log10(P);
+end
+
+hSpectrum = figure;
+plot(F, mean(Parr));
+hold on; % for others!
+xlabel("Frequency (Hz)")
+ylabel("Mean Power")
+title("Mean Power against Frequency (Wake-Still)")
+% extract freq band
+bandPowers = Parr(:,F>1 & F<4);
+% take mean of dim=2
+bandPower = mean(bandPowers,2);
+% make sure it's relatively stable across each bin
+figure;
+plot(bandPower);
+title("Mean Power at 2Hz (Wake-Still)")
 
 
 
