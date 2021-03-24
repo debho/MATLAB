775e99ee1ca205ec5faccd5df0f4a59114704e92
Matt Gaidica <matt@gaidi.ca>
2021-03-24 08:57:34 -0400

    Update mouse_combined.m

--- mouse_combined.m
+++ mouse_combined.m
@@ -1,16 +1,25 @@
+usePath = '/Users/matt/Downloads';
+
 close all; % closes all figures, can probably take this out once i'm done fixing code
+% make figures BEFORE loop
+hspectrum = ff(1200,800);
+hpower = ff(1200,800);
 for i = 1:2
+    % !! you're not saving individual variables, you're saving the entire
+    % workspace. Read my note again: save('myfile.mat','variable1') then use load('myfile.mat') in a loop
+    % only save variables you will reuse, these files are gigantic
     if(i==1)
-        load('20210108_var.mat')
+        load(fullfile(usePath,'20210108_var.mat'))
     else
-        load('20210311_var.mat')
+        load(fullfile(usePath,'20210311_var.mat'))
     end
     % do plots   
     % SPECTROGRAM
     % current issues: spectrograms still showing separately
-    figure('position',[0 0 2800 1000]);
-   
-    subplot(1,2,i);
+    
+    % select figures like this to plot multiple things
+    figure(hspectrum);
+    subplot(2,1,i);
     pspectrum(eeg_t,"spectrogram","FrequencyLimits",[1 30]);
     colormap(jet);
     if(i==1)
@@ -19,6 +28,11 @@ for i = 1:2
         caxis auto;
     end
     title(sprintf("Spectrogram for Day %i",i));
+    
+    figure(hpower);
+    subplot(2,1,i);
+    % !! this is funny business, I would try to fix these things before saving
+    % the .mat file
     if(i==1)
         bTime = behTime/60/60;
     else
@@ -37,6 +51,7 @@ for i = 1:2
     set(gca,'fontsize',14);
     set(gcf,'color','w');
     yticks([]);
+    
     drawnow;
 end
 
