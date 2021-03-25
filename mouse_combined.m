3cb8697505c8061442acccb58333d60d0b89ec6d
Deborah Ho <47065259+debho@users.noreply.github.com>
2021-03-25 15:21:05 -0400

    added code to compare sleep between days

--- mouse_combined.m
+++ mouse_combined.m
@@ -44,8 +44,54 @@ for i = 1:2
     
     %% POWER SPECTRA
     figure(hpower);
+    subplot(1,2,i);
+    
+    %% SAVE VARIABLES INTO NEW ONES FOR ANALYSIS
+    if(i==1)
+        Parr_sleep1 = Parr_sleep;
+        Parr_wake_still1 = Parr_wake_still;
+    else
+        Parr_sleep2 = Parr_sleep;
+        Parr_wake_still2 = Parr_wake_still;
+    end
     
 end
 
-% STATISTICAL ANALYSIS
- 
+%% STATISTICAL ANALYSIS
+% sleep
+pvalue_at_F = NaN(size(F));
+for iFreq = 1:numel(F)
+    x = [Parr_sleep1(:,iFreq);Parr_sleep2(:,iFreq)];
+    groups = [zeros(size(Parr_sleep1(:,iFreq)));ones(size(Parr_sleep2(:,iFreq)))];
+    pvalue_at_F(iFreq) = anova1(x,groups,'off');
+end
+
+lw = 2;
+hSpectrum = ff(1400,500);
+usexlims = [[0,100];[0,10]];
+for iPlot = 1:2
+    subplot(1,2,iPlot);
+    plot(F,mean(Parr_sleep1),'linewidth',lw);
+    hold on;
+    xlabel("Frequency (Hz)")
+    ylabel("Mean Power")
+    plot(F, mean(Parr_sleep2),'linewidth',lw);
+    set(gca,'fontsize',16);
+    grid on;
+
+    pThresh = [0.001, 0.01, 0.05];
+    colors = gray(4);
+    plotAt = max(ylim); % just find a place to plot the asterik
+    for iThresh = 1:numel(pThresh)
+        useXlocs = find(pvalue_at_F < pThresh(iThresh));
+        plot(F(useXlocs),ones(size(useXlocs))*(plotAt-iThresh+1),'*','color',colors(iThresh,:));
+    end
+
+    xlim(usexlims(iPlot,:));
+    if iPlot == 1
+        title("Mean Power against Frequency, Sleep");
+        legend({'Day 1','Day 2','p < 0.001','p < 0.01','p < 0.05'},'location','southwest');
+    else
+        title('Zoomed-in on low frequencies');
+    end
+end
